051fc94d97ff9f14943e55ecf9d07b5dc33f495e
naayla <naylasyifa3006@gmail.com>
2024-09-12 20:47:26 +0700

    Add files via upload

---
+++ DASPRO_2.pas
@@ -0,0 +1,19 @@
+program DaftarNamaBulan;
+uses crt;
+begin
+  clrscr;  
+  writeln('Daftar Nama Bulan:');
+  writeln('Januari');
+  writeln('Februari');
+  writeln('Maret');
+  writeln('April');
+  writeln('Mei');
+  writeln('Juni');
+  writeln('Juli');
+  writeln('Agustus');
+  writeln('September');
+  writeln('Oktober');
+  writeln('November');
+  writeln('Desember');
+  readln;  
+end.
