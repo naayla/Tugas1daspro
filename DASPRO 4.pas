b4f931e234997f8d83965be081907ad29d31eaf8
naayla <naylasyifa3006@gmail.com>
2024-09-14 21:40:32 +0700

    Add files via upload

---
+++ DASPRO 4.pas
@@ -0,0 +1,17 @@
+program AlamatLengkap;
+
+var
+  jalan, kota: string;
+  no, kodePos: string;
+
+begin
+  write('Masukkan nama jalan: ');
+  readln(jalan);
+  write('Masukkan nomor: ');
+  readln(no);
+  write('Masukkan kota: ');
+  readln(kota);
+  write('Masukkan kode pos: ');
+  readln(kodePos);
+  writeln('Alamat: Jalan ', jalan, ' No. ', no, ' ', kota, ' ', kodePos);
+end.
